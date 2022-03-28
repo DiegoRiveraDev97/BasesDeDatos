64844d6cca874d1ffb4f835e205c1408ee4b0d06
Juan Diego Rivera Meneses <jdrivera847@misena.edu.co>
2022-03-28 09:37:03 -0500

    bbdd_exercise

---
+++ inventado_diego1.sql
@@ -0,0 +1,7 @@
+show databases;
+use taller_inventado_diego;
+show tables;
+explain  alquiler;
+insert into alquiler(propietario_cod_pro,parqueadero_cod_par,fec_alq,lug_alq,cod_alq)values
+("1b","11wb22","2020-09-09","bogota","8080");
+select * from alquiler;
